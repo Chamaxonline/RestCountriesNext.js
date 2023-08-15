da0c53af3f362515c35fe13a1fe6f5a2a1c13089
Chamath Viduranga <chamath.viduranga@airudi.com>
2023-08-16 00:04:48 +0530

    fix the issue

--- src/rest-countries/pages/api/countries/country.ts
+++ src/rest-countries/pages/api/countries/country.ts
@@ -3,18 +3,26 @@ import countriesData from "../../../data/countries.json"; // Update the path to 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "GET") {
-    const { countryName } = req.query;
-
-    if (countryName) {
-      const selectedCountry = countriesData.find(
-        (country) =>
-          country.name.common.toLowerCase() === countryName.toLowerCase()
-      );
-      return res.status(200).json(selectedCountry);
+    if (req.method === 'GET') {
+      const { countryName } = req.query;
+  
+      if (typeof countryName === 'string') {
+        const selectedCountry = countriesData.find(
+          (country) =>
+            country.name.common.toLowerCase() === countryName.toLowerCase()
+        );
+  
+        if (selectedCountry) {
+          return res.status(200).json(selectedCountry);
+        } else {
+          return res.status(404).json({ message: 'Country not found' });
+        }
+      } else {
+        return res.status(400).json({ message: 'Invalid input' });
+      }
     } else {
-      return res.status(405).json({ message: "Method not allowed" });
+      return res.status(405).json({ message: 'Method not allowed' });
     }
-  }
-};
+  };
+  
 //http://localhost:3000/api/countries/country?countryName=india
--- src/rest-countries/pages/api/countries/search.ts
+++ src/rest-countries/pages/api/countries/search.ts
@@ -6,12 +6,16 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     const { name } = req.query;
 
-    if (name) {
+    if (typeof name === 'string') {
       const searchResults = countriesData.filter((country) =>
         country.name.common.toLowerCase().includes(name.toLowerCase())
       ).map(x=> x.name.common);;
 
-      return res.status(200).json(searchResults);
+      if (searchResults) {
+        return res.status(200).json(searchResults);
+      } else {
+        return res.status(404).json({ message: 'Country not found' });
+      }
     } else {
       return res.status(405).json({ message: "Method not allowed" });
     }
